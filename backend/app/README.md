336dc96b4f1d41a881bffb2c981ed10a8832f048
Kamran Abdi <kamranabdicse@gmail.com>
2022-12-27 12:07:29 -0800

    feat: config fastapi with docker compose
    we make a structure for this project

---
+++ backend/app/README.md
@@ -0,0 +1,20 @@
+## Setup project
+create poetry
+```
+curl -sSL https://install.python-poetry.org | python3 -
+```
+```
+cd backend
+```
+```
+poetry new app
+```
+```
+pip3 install uvicorn gunicorn fastapi
+```
+```
+cd /backend/app
+```
+```
+
+```
---
+++ start-server.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env sh
+set -e
+
+########################
+### run with uvicorn ###
+########################
+# exec uvicorn --reload --host 0.0.0.0 --port 80 --log-level info "app.main:app"
+
+#########################
+### run with gunicorn ###
+#########################
+export GUNICORN_CONF="/gunicorn_conf.py"
+export WORKER_CLASS="uvicorn.workers.UvicornWorker"
+export WORKERS_NUM=1
+export APP_MODULE="app.main:app"
+exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" --workers $WORKERS_NUM $APP_MODULE
