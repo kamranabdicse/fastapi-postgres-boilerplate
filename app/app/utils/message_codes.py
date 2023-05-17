3999f460b00a586e58b23a3c723a11dcf64e962d
Ali Samani <a.samani@gmail.com>
2023-05-18 01:09:58 +0330

    messagecode updated

--- app/app/utils/message_codes.py
+++ app/app/utils/message_codes.py
@@ -9,6 +9,8 @@ class MessageCodes:
     bad_request = 3
     input_error = 4
     operation_failed = 5
+    incorrect_email_or_password = 6
+    inactive_user = 7
     # services code start from 1001
 
     messages_names = {
@@ -18,6 +20,9 @@ class MessageCodes:
         3: "Bad Request",
         4: "Input Error",
         5: "Operation Failed",
+        6: "Invalid Email Or Password",
+        7: "Inactive User",
+
     }
 
     persian_message_names = {
@@ -27,4 +32,6 @@ class MessageCodes:
         3: "درخواست نا‌معتبر",
         4: "ورودی نامعتبر",
         5: "عملیات ناموفق",
+        6: "ایمیل یا پسورد نامعتبر",
+        7: "یوزر غیرفعال",
     }
