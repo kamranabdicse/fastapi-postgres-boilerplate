1505e23ccf037f49e2b102d1dc3d67761de5d632
Amir <mailwork368@gmail.com>
2023-08-10 11:09:32 -0400

    fix: remove aioredis and use redis lib for asyncio client

--- app/cache/redis.py
+++ app/cache/redis.py
@@ -2,12 +2,11 @@
 import os
 from typing import Tuple
 
-import aioredis
-
+import redis.asyncio as redis
 from cache.enums import RedisStatus
 
 
-async def redis_connect(host_url: str) -> Tuple[RedisStatus, aioredis.client.Redis]:
+async def redis_connect(host_url: str) -> Tuple[RedisStatus, redis.Redis]:
     """Attempt to connect to `host_url` and return a Redis client instance if successful."""
     return (
         await _connect(host_url)
@@ -18,19 +17,19 @@ async def redis_connect(host_url: str) -> Tuple[RedisStatus, aioredis.client.Red
 
 async def _connect(
     host_url: str,
-) -> tuple[RedisStatus, aioredis.client.Redis]:  # pragma: no cover
+) -> tuple[RedisStatus, redis.Redis]:  # pragma: no cover
     try:
-        redis_client = await aioredis.from_url(host_url)
+        redis_client = await redis.from_url(host_url)
         if await redis_client.ping():
             return (RedisStatus.CONNECTED, redis_client)
         return (RedisStatus.CONN_ERROR, None)
-    except aioredis.AuthenticationError:
+    except redis.AuthenticationError:
         return (RedisStatus.AUTH_ERROR, None)
-    except aioredis.ConnectionError:
+    except redis.ConnectionError:
         return (RedisStatus.CONN_ERROR, None)
 
 
-def _connect_fake() -> Tuple[RedisStatus, aioredis.client.Redis]:
+def _connect_fake() -> Tuple[RedisStatus, redis.Redis]:
     from fakeredis import FakeRedis
 
     return (RedisStatus.CONNECTED, FakeRedis())
